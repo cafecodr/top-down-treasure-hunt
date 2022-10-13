8967570fe9439db05f7d7575b9d2566a9e4213d1
Andrew Pomerleau <cafecodr@gmail.com>
2022-10-13 19:37:57 -0400

    Build basic JFrame and JPanel (`GamePanel`) to display the game.

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,27 @@
+package main;
+
+import javax.swing.*;
+
+public class Main {
+    final static String NAME = "2D Adventure";
+    final static int MAJOR = 0;
+    final static int MINOR = 0;
+    final static int COMMIT = 1;
+    final static String TITLE = String.format("%s v%d.%d.%d",
+            NAME, MAJOR, MINOR, COMMIT);
+
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle(TITLE);
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+    }
+}
