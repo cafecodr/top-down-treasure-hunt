348753374bf467616fb4b1444bdc7207ae9ff888
Andrew Pomerleau <cafecodr@gmail.com>
2022-10-21 14:03:35 -0400

    Refactored to assign Tile objects into a HashMap

--- src/main/java/tiles/TileManager.java
+++ src/main/java/tiles/TileManager.java
@@ -6,19 +6,20 @@ import java.awt.*;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+
 
 public class TileManager {
     static final int I_GRASS = 0, I_WALL = 1, I_WATER = 2, I_EARTH = 3, I_TREE = 4, I_SAND = 5;
     GamePanel gp;
-    public Tile[] tiles;
+    private HashMap<Integer, Tile> tiles;
     public int[][] mapTileNum;
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
-        tiles = new Tile[10];
         mapTileNum = new int[gp.maxWorldCol][gp.maxWorldRow];
 
-        getTileImage();
+        loadTiles();
         loadMap("world01");
     }
 
@@ -53,14 +54,16 @@ public class TileManager {
         }
     }
 
-    public void getTileImage() {
-        tiles[I_GRASS] = new TlGrass();
-        tiles[I_WALL] = new TlWall();
-        tiles[I_WATER] = new TlWater();
+    public void loadTiles() {
+        tiles = new HashMap<>();
 
-        tiles[I_EARTH] = new TlEarth();
-        tiles[I_TREE] = new TlTree();
-        tiles[I_SAND] = new TlSand();
+        tiles.put(I_GRASS, new TlGrass());
+        tiles.put(I_WALL, new TlWall());
+        tiles.put(I_WATER, new TlWater());
+
+        tiles.put(I_EARTH, new TlEarth());
+        tiles.put(I_TREE, new TlTree());
+        tiles.put(I_SAND, new TlSand());
     }
 
     public void draw(Graphics2D g2d) {
@@ -77,7 +80,7 @@ public class TileManager {
             int screenY = worldY - gp.player.worldY + gp.player.screenY;
 
             if (gp.isOnScreen(worldX, worldY)) {
-                g2d.drawImage(tiles[tileNum].getImage(), screenX, screenY, gp.tileSize, gp.tileSize, null);
+                g2d.drawImage(tiles.get(tileNum).getImage(), screenX, screenY, gp.tileSize, gp.tileSize, null);
             }
             worldCol++;
 
